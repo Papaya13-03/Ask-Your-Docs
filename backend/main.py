21a7800a795875609adcedd27ce5cb5132314565
Du Van Nguyen <du@Du.localdomain>
2026-01-12 10:05:47 +0700

    fix: failed to build

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from backend!")
+import uvicorn
+from app.main import app
 
+def main():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
     main()
